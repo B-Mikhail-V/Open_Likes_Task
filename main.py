3f272af351c05343dcf7e9ff534fbfd174281de6
Mikail Bakulin <bakulin.mv@gmail.com>
2021-11-23 18:13:39 +0300

    Coding ver_2

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ with open(path, encoding='utf-8') as dishes:
             # igredient = dishes.readline().split(" | ")
             temp_list = []
             igredient_name, igredient_quantity, igredient_measure = dishes.readline().split(" | ")
-            print (igredient_name, igredient_quantity, gredient_measure)
-            temp_list.append({'ingredient_name' = igredient_name, 'quantity' =  gredient_quantity, 'measure' = gredient_measure})
+            # print (igredient_name, igredient_quantity, igredient_measure)
+            temp_list.append({'ingredient_name' : igredient_name, 'quantity' :  igredient_quantity, 'measure' : igredient_measure})
+            print(temp_list)
         dishes.readline().strip()
