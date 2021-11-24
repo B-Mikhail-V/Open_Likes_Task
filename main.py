0adaffc8dbb960da30ff070b462f7871035919c6
Mikail Bakulin <bakulin.mv@gmail.com>
2021-11-24 18:58:05 +0300

    Coding ver_4

--- main.py
+++ main.py
@@ -22,16 +22,27 @@ with open(path, encoding='utf-8') as dishes:
 
 def get_shop_list_by_dishes(dishes, person_count):
     dishes_total = dishes * person_count
-    ingredient_order_list = []
+    ingredient_order_list = {}
     for key, value in result.items():
+        ing_dict = {}
         if key in dishes_total:
-            ing_dict = {}
+            # ing_dict = {}
             for ingredient in value:
                 # print(ingredient['ingredient_name'])
                 ingredient_quantity_order = ingredient['quantity']
                 # print(ingredient['ingredient_name'], ingredient_quantity_order, ingredient['measure'])
                 list_temp = [ingredient['ingredient_name'],]
                 ing_dict[ingredient['ingredient_name']] = [{'quantity':  ingredient['quantity'], 'measure':  ingredient['measure']}]
-            pprint(ing_dict)
+                if ingredient in value:
+                    print(f"{ingredient} Есть такой {value}")
 
-print(get_shop_list_by_dishes(['Омлет', 'Утка по-пекински'], 2))
+                    print(f"{ingredient} Есть такой {value}")
+
+                else:
+                    print(f"{ingredient['ingredient_name'] } Нет такого {value}")
+            # pprint(ing_dict)
+                ingredient_order_list[ingredient['ingredient_name']] = ingredient_order_list.get(ingredient['ingredient_name'], [{'quantity':  ingredient['quantity'], 'measure':  ingredient['measure']}])
+    pprint(ingredient_order_list)
+
+
+print(get_shop_list_by_dishes(['Омлет', 'Утка по-пекински', 'Фахитос', 'Фахитос_2'], 2))
