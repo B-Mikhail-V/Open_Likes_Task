96e79cc3d5d6ad264e66b97e2f4215247f06d913
Mikhail Bakulin <bakulin.mv@gmail.com>
2021-11-26 23:37:00 +0300

    Final set

--- main_2.py
+++ main_2.py
@@ -75,19 +75,29 @@
 
 import os
 from pprint import pprint
+
 files_list = ['1.txt', '2.txt', '3.txt']
-line_count_list = []
+line_count_list = [] #
 dict_texts = {}
+dict_files = {}
 for file in files_list:
     path = os.path.join(os.getcwd(), file)
     with open(path, encoding='utf-8') as text:
         text_text = text.readlines()
         line_count = len(text_text)
         line_count_list.append(line_count)
-        dict_texts = {line_count: text_text}
+        dict_texts.update({line_count: text_text})
+        dict_files.update({line_count: file})
         # pprint(line_count)
         # pprint(text_text)
-pprint(dict_texts)
-pprint(sorted(line_count_list))
-
-
+# pprint(dict_texts)
+# pprint(dict_files)
+# pprint(sorted(line_count_list))
+path_write = os.path.join(os.getcwd(), 'sorted_list.txt')
+with open(path_write, "a", encoding='utf-8') as file:
+    for count in sorted(line_count_list):
+        file.write(dict_files[count] + "\n")
+        file.write(str(sorted(line_count_list).index(count) + 1) + "\n")
+        for line in dict_texts[count]:
+            file.write(line)
+        file.write('\n')
